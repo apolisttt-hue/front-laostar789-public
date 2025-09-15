1ee5fba49b43d826f068da85a9a73371d45950b5
ErrorCode52 <watunyu.man@gmail.com>
2025-09-15 20:38:59 +0700

    upload all file

---
+++ app/layout.js
@@ -0,0 +1,31 @@
+// app/layout.js
+import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Application Base game entertainment",
+  description: "Application Base game entertainment",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,60 @@
+// app/page.js
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [lang, setLang] = useState("la");
+
+  const text = {
+    th: {
+      login: "เข้าสู่ระบบ",
+      register: "สมัครสมาชิก",
+    },
+    la: {
+      login: "ເຂົ້າລະບົບ",
+      register: "ລົງທະບຽນສະມາຊິກ",
+    },
+  };
+
+  const getLogin = () => {
+    window.location.href = "https://app.laostar789.net/";
+  };
+  const getRegister = () => {
+    window.location.href = "https://app.laostar789.net/register";
+  };
+
+  return (
+    <div className="min-h-screen bg-[url('/bg.jpg')] bg-cover bg-center text-white relative">
+      {/* dropdown เปลี่ยนภาษา */}
+      <div className="absolute top-4 right-4 z-10 bg-gray-300 bg-opacity-50 rounded-md p-2">
+        <select
+          value={lang}
+          onChange={(e) => setLang(e.target.value)}
+          className="text-black px-3 py-1 rounded-md shadow"
+        >
+          <option value="th">🇹🇭 ไทย</option>
+          <option value="la">🇱🇦 ລາວ</option>
+        </select>
+      </div>
+
+      {/* โลโก้ + ปุ่ม */}
+      <div className="container mx-auto px-4 py-24 text-center flex flex-col items-center justify-center min-h-screen">
+        <Image
+          src="/logo.gif"
+          alt="Logo"
+          width={450}
+          height={450}
+          className="mx-auto mb-8"
+        />
+        <button onClick={getLogin} className="button-like-glow mb-4">
+          {text[lang].login}
+        </button>
+        <button onClick={getRegister} className="button-like-glow">
+          {text[lang].register}
+        </button>
+      </div>
+    </div>
+  );
+}
